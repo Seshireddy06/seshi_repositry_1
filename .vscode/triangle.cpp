37741f677d847ef5b89889d126508f4e716caff3
seshireddy <seshireddyustulumuri@gmail.com>
2025-07-31 14:59:00 +0530

    add two triangles

---
+++ .vscode/triangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+cout << "\nEquilateral triangle pattern:\n";
+for(int i = 1; i <= n; ++i) {
+    // Print spaces
+    for(int s = 1; s <= n - i; ++s) {
+        cout << "  ";
+    }
+    // Print stars
+    for(int j = 1; j <= 2 * i - 1; ++j) {
+        cout << "* ";
+    }
+    cout << endl;
+}
+return 0;
+}
