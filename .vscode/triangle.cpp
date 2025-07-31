eb6d6e6b04a9dae628ce310de625065fc91b0b7a
seshireddy <seshireddyustulumuri@gmail.com>
2025-07-31 15:13:22 +0530

    two triangles

--- .vscode/triangle.cpp
+++ .vscode/triangle.cpp
@@ -6,24 +6,26 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> n;
 
+    // Left-aligned triangle
     for(int i = 1; i <= n; ++i) {
         for(int j = 1; j <= i; ++j) {
-            cout << j << " ";
+            cout << "*";
         }
         cout << endl;
     }
     
-cout << "\nEquilateral triangle pattern:\n";
-for(int i = 1; i <= n; ++i) {
-    // Print spaces
-    for(int s = 1; s <= n - i; ++s) {
-        cout << "  ";
+    // Equilateral triangle
+    cout << "\nEquilateral triangle pattern:\n";
+    for(int i = 1; i <= n; ++i) {
+        // Print spaces
+        for(int s = 1; s <= n - i; ++s) {
+            cout << "  ";
+        }
+        // Print stars
+        for(int j = 1; j <= 2 * i - 1; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    // Print stars
-    for(int j = 1; j <= 2 * i - 1; ++j) {
-        cout << "* ";
-    }
-    cout << endl;
-}
-return 0;
+    return 0;
 }
